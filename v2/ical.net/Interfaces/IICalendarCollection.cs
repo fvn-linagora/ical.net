25d2ac5c7256db868a57d8c9f84d1252d46d8c87
Rian Stockbower <rstockbower@gmail.com>
2016-10-01 16:02:03 -0400

    IICalendarCollection is a pointless interface

--- v2/ical.net/Interfaces/IICalendarCollection.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-using ical.net.Interfaces.Evaluation;
-
-namespace ical.net.Interfaces
-{
-    public interface IICalendarCollection : IGetOccurrencesTyped, IList<ICalendar> {}
-}
