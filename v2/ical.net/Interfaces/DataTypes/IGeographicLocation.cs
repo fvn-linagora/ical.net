e4d8798fe034dafa0596425e8d373849ab1c03aa
Rian Stockbower <rstockbower@gmail.com>
2016-10-02 12:04:02 -0400

    IGeographicLocation is a pointless interface

--- v2/ical.net/Interfaces/DataTypes/IGeographicLocation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ical.net.Interfaces.DataTypes
-{
-    public interface IGeographicLocation : IEncodableDataType
-    {
-        double Latitude { get; set; }
-        double Longitude { get; set; }
-    }
-}
--- v2/ical.net/Serialization/iCalendar/Factory/DataTypeSerializerFactory.cs
+++ v2/ical.net/Serialization/iCalendar/Factory/DataTypeSerializerFactory.cs
@@ -41,7 +41,7 @@ namespace ical.net.Serialization.iCalendar.Factory
                 {
                     s = new FreeBusyEntrySerializer();
                 }
-                else if (typeof (IGeographicLocation).IsAssignableFrom(objectType))
+                else if (typeof (GeographicLocation).IsAssignableFrom(objectType))
                 {
                     s = new GeographicLocationSerializer();
                 }
